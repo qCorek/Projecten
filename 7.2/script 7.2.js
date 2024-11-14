1864b4538c96e78fb7211af6611ca8e2adababac
Ali <l4nxgt@gmail.com>
2024-11-14 13:14:07 +0100

    Add files via upload

---
+++ 7.2/script 7.2.js
@@ -0,0 +1,13 @@
+let budget = 300;
+
+let product = prompt("Hoe duur is uw product? ");
+if (product < budget){
+    document.getElementById("tekst").style.color = "green";
+    document.getElementById("tekst").textContent = "U heeft genoeg geld!";
+    alert("U heeft genoeg geld!");
+}
+else {
+    document.getElementById("tekst").style.color = "red";
+    document.getElementById("tekst").textContent = "U heeft niet genoeg geld!";
+    alert("U heeft niet genoeg geld!");
+}
