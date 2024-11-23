bd9f653f8b6c3d4b606ac70b96264699cce70ec2
Ali <l4nxgt@gmail.com>
2024-11-23 02:16:03 +0100

    Add files via upload

---
+++ PHP_Opdracht2/index.php
@@ -0,0 +1,22 @@
+<?php
+
+//  Een voorbeeld van een echo en een print
+echo "<h1>Hallo wereld,</h1>";
+echo "Ik zit op het ";
+
+// Voorbeeld van een variabele in PHP.
+$schoolnaam = "Techniek College Rotterdam !";
+
+// Met echo kun je iets op het scherm tonen.
+echo "$schoolnaam"."\n";
+
+
+
+
+
+//een voorbeeld hoe je tot 100 kan laat printen zonder heletijd echo te gebruiken.
+
+$num = 0;
+while($num < 101)
+	echo($num++ . "\n")
+?>
