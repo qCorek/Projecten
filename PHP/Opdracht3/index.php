9abce2a49ce1f99e7bcde984b97b70a4445d28eb
Ali <l4nxgt@gmail.com>
2024-11-26 22:44:47 +0100

    Add files via upload

---
+++ PHP/Opdracht3/index.php
@@ -0,0 +1,12 @@
+<?php
+$string1 = "Hallo";
+$string2 = "Wereld";
+
+echo $string1 . " " . $string2 . "<br>";
+
+$getal1 = 10;
+$getal2 = 20;
+
+print "string"; "Het eerste getal is: " . $getal1 . "<br>";
+print "Het tweede getal is: " . $getal2 . "<br>";
+?>
